c576868a43733721ffea0feec788b6e9520d4e36
Zhu Zijun <2961163526@qq.com>
2023-05-09 00:13:48 +0800

    增加天空盒 增加草地、道路材质 添加生成道路的相关代码

--- front/src/components/MainVisualizationView.tsx
+++ front/src/components/MainVisualizationView.tsx
@@ -1,43 +1,32 @@
 import React from 'react';
 import {
     Vector3,
-    HemisphericLight,
-    MeshBuilder,
-    Mesh,
     Scene,
     Vector2,
-    ArcRotateCamera
+    ArcRotateCamera, DirectionalLight
 } from '@babylonjs/core';
 import SceneComponent from 'babylonjs-hook';
-import {creatRoad} from "../utils/roadVisualizeHelp.ts";
-
-let box: Mesh;
+import {creatGround, creatRoad, creatSkyBox} from "../utils/roadVisualizeHelp.ts";
 
 const onSceneReady = (scene: Scene) => {
     const camera = new ArcRotateCamera("camera", 0, 0, 10, Vector3.Zero());
     camera.setTarget(Vector3.Zero());
+    camera.upperBetaLimit = 5 * Math.PI / 12;
+    camera.lowerRadiusLimit = 3;
+    camera.upperRadiusLimit = 120;
 
     const canvas = scene.getEngine().getRenderingCanvas();
     camera.attachControl(canvas, true);
 
-    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    const light = new DirectionalLight("light", new Vector3(0.1, -1, 0), scene);
     light.intensity = 0.7;
 
-    box = MeshBuilder.CreateBox("box", {size: 2}, scene);
-    box.position.y = 1;
-
-    // MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
-
-    creatRoad(scene, new Vector2(1, 1), new Vector2(2, 2));
+    creatGround(scene);
+    creatRoad(scene, new Vector2(0, 0), new Vector2(10, 8));
+    creatSkyBox(scene);
 }
 
-const onRender = (scene: Scene) => {
-    if (box !== undefined) {
-        var deltaTimeInMillis = scene.getEngine().getDeltaTime();
-
-        const rpm = 10;
-        box.rotation.y += ((rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000));
-    }
+const onRender = (_: Scene) => {
 }
 
 const MainVisualizationView: React.FC = () => {
--- front/src/utils/roadVisualizeHelp.ts
+++ front/src/utils/roadVisualizeHelp.ts
@@ -1,5 +1,5 @@
 import {Scene, MeshBuilder, Vector3, Vector2, Plane} from "@babylonjs/core";
-import {calculateDistance2D} from "./tools.ts";
+import {calculateDistance2D, creatGrassMaterial, creatRoadMaterial, creatSkyboxMaterial} from "./tools.ts";
 
 export function creatRoad(scene: Scene, start: Vector2, end: Vector2) {
     const length = calculateDistance2D(start, end);
@@ -12,6 +12,17 @@ export function creatRoad(scene: Scene, start: Vector2, end: Vector2) {
             width: 1, height: length, sourcePlane: sourcePlane
         }, scene);
 
-    road.position = new Vector3(start.x, 0, start.y);
+    road.position = new Vector3(start.x, 0.01, start.y);
     road.addRotation(0, 0, Math.atan((end.y - start.y) / (end.x - start.x)));
+    road.material = creatRoadMaterial(scene, length);
+}
+
+export function creatGround(scene: Scene) {
+    const ground = MeshBuilder.CreateGround("ground", {width: 500, height: 500}, scene);
+    ground.material = creatGrassMaterial(scene, 500, 500);
+}
+
+export function creatSkyBox(scene: Scene) {
+    const skybox = MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+    skybox.material = creatSkyboxMaterial(scene);
 }
