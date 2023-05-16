97144a92aebd79c8440ab89ed821cfded94497cf
Cherruy <1312869838@qq.com>
2023-05-16 21:40:30 +0800

    1

--- front/src/components/secondComponents/roadMap/roadMap.tsx
+++ front/src/components/secondComponents/roadMap/roadMap.tsx
@@ -7,37 +7,46 @@ const RoadMap = () => {
     const mapRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         if (mapRef.current) {
-
-            // 创建 Leaflet 地图实例，并设置地图的初始视图
-            const map = L.map(mapRef.current).setView([51.505, -0.09], 13);
-            // 添加EPSG4326转换器
-            L.Proj.geoJson(null, {
-                // 指定源坐标系为EPSG4326
-                crs: L.CRS.EPSG4326,
-            }).addTo(map);
-
-            // 添加地图图层
+            var map = L.map(mapRef.current,{
+                crs:L.CRS.EPSG4326
+            }).setView([15.538926, 114.354382], 1);
             L.tileLayer('', {  // 不使用瓦片地图
                 attribution: '',
                 maxZoom: 18,
             }).addTo(map);
+            loadRoadData(map);
+            
+            // 创建 Leaflet 地图实例，并设置地图的初始视图
+            // const map = L.map(mapRef.current).setView([15.538926, 114.354382], 1);
+            // 添加EPSG4326转换器
+            // L.Proj.geoJson(null, {
+            //     // 指定源坐标系为EPSG4326
+            //     crs: L.CRS.EPSG4326,
+            // }).addTo(map);
+
+            // 添加地图图层
+            // L.tileLayer('', {  // 不使用瓦片地图
+            //     attribution: '',
+            //     maxZoom: 18,
+            // }).addTo(map);
 
             // 加载并绘制道路数据
-            loadRoadData(map);
+            // loadRoadData(map);
         }
     }, []);
     const loadRoadData = async (map: L.Map) => {
         const loadGeoJSON = async (url: string): Promise<L.GeoJSON> => {
             const response = await fetch(url);
             const data = await response.json();
-            // 进行EPSG4326到EPSG3857的坐标系转换
-            const transformedData = L.Proj.geoJson(data, {
-                // 指定源坐标系为EPSG4326
-                fromCRS: L.CRS.EPSG4326,
-                // 指定目标坐标系为EPSG3857
-                toCRS: map.options.crs,
-            });
-            return L.geoJSON(transformedData);
+            // // 进行EPSG4326到EPSG3857的坐标系转换
+            // const transformedData = L.Proj.geoJson(data, {
+            //     // 指定源坐标系为EPSG4326
+            //     fromCRS: L.CRS.EPSG4326,
+            //     // 指定目标坐标系为EPSG3857
+            //     toCRS: map.options.crs,
+            // });
+            // return L.geoJSON(transformedData);
+            return L.geoJSON(data);
         };
         try {
             // const crosswalkRoad = await loadGeoJSON("../../../assets/roadData/crosswalkroad_with9road.geojson");
@@ -49,7 +58,10 @@ const RoadMap = () => {
             // laneRoad.addTo(map);
             // signalRoad.addTo(map);
             // stoplineRoad.addTo(map);
-            boundaryRoad.addTo(map);
+            // boundaryRoad.addTo(map);
+            L.geoJSON(boundaryRoad, {
+                // onEachFeature: myOnEachFeature,
+            }).addTo(Map);
         } catch (error) {
             console.error('Failed to load road data:', error);
         }
